4482d7fca9df5ae85dbda7c886d666fbc35248cd
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-06 17:38:10 +0000

    Change Log

--- src/Application/Command/BidStrategyDeleteCommand.php
+++ src/Application/Command/BidStrategyDeleteCommand.php
@@ -2,4 +2,32 @@
 
 declare(strict_types=1);
 
-namespace App\Appl
+namespace App\Application\Command;
+
+use App\Application\DTO\BidStrategyDeleteDTO;
+use App\Domain\Repository\BidStrategyRepository;
+use Psr\Log\LoggerInterface;
+
+final class BidStrategyDeleteCommand
+{
+    private BidStrategyRepository $bidStrategyRepository;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        BidStrategyRepository $bidStrategyRepository,
+        LoggerInterface $logger
+    ) {
+        $this->bidStrategyRepository = $bidStrategyRepository;
+        $this->logger = $logger;
+    }
+
+    public function execute(BidStrategyDeleteDTO $dto): int
+    {
+        $this->logger->debug('Running delete bid strategy command');
+        $result = $this->bidStrategyRepository->deleteAll($dto->getIds());
+        $this->logger->info(sprintf('%d bid strategies deleted', $result));
+
+        return $result;
+    }
+}
