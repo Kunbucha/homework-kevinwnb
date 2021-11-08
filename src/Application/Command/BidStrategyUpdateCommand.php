bd4b75f63ca93c7d325438c543b46695ebbc6c70
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-08 05:39:19 +0000

    Updated distribution files

--- src/Application/Command/BidStrategyUpdateCommand.php
+++ src/Application/Command/BidStrategyUpdateCommand.php
@@ -5,4 +5,29 @@ declare(strict_types=1);
 namespace App\Application\Command;
 
 use App\Application\DTO\BidStrategyUpdateDTO;
-use App\Domain
+use App\Domain\Repository\BidStrategyRepository;
+use Psr\Log\LoggerInterface;
+
+final class BidStrategyUpdateCommand
+{
+    private BidStrategyRepository $bidStrategyRepository;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        BidStrategyRepository $bidStrategyRepository,
+        LoggerInterface $logger
+    ) {
+        $this->bidStrategyRepository = $bidStrategyRepository;
+        $this->logger = $logger;
+    }
+
+    public function execute(BidStrategyUpdateDTO $dto): int
+    {
+        $this->logger->debug('Running update bid strategy command');
+        $result = $this->bidStrategyRepository->saveAll($dto->getBidStrategies());
+        $this->logger->info(sprintf('%d bid strategies updated', $result));
+
+        return $result;
+    }
+}
---
+++ src/Application/Command/CampaignDeleteCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Command;
+
+use App\Application\DTO\CampaignDeleteDTO;
+use App\Domain\Repository\CampaignRepository;
+use Psr\Log\LoggerIn
