695d7871217fed4214cca1a96d5b35bfff2b1cfd
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-08 10:26:12 +0000

    TODO done

--- src/Application/Command/EventDeleteCommand.php
+++ src/Application/Command/EventDeleteCommand.php
@@ -9,4 +9,26 @@ use App\Domain\ValueObject\EventType;
 use DateTimeInterface;
 use Psr\Log\LoggerInterface;
 
-final class EventDeleteComman
+final class EventDeleteCommand
+{
+    private EventRepository $eventRepository;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        EventRepository $eventRepository,
+        LoggerInterface $logger
+    ) {
+        $this->eventRepository = $eventRepository;
+        $this->logger = $logger;
+    }
+
+    public function execute(DateTimeInterface $dateTo): int
+    {
+        $this->logger->debug('Running delete events command');
+        $views = $this->eventRepository->deleteByTime(EventType::createView(), null, $dateTo);
+        $this->logger->info(sprintf('%d views deleted', $views));
+        $clicks = $this->eventRepository->deleteByTime(EventType::createClick(), null, $dateTo);
+        $this->logger->info(sprintf('%d clicks deleted', $clicks));
+        $conversions = $this->eventRepository->deleteByTime(EventType::createConversion(), null, $dateTo);
+        $this->logger->info(sprintf('%d conversions delete
