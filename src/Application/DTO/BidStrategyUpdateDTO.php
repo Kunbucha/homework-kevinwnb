0df8f76ecf16de63ef8b901a15fb4af400376e79
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-18 14:42:43 +0000

    updated CHANGELOG for 2.1

--- src/Application/DTO/BidStrategyUpdateDTO.php
+++ src/Application/DTO/BidStrategyUpdateDTO.php
@@ -21,4 +21,35 @@ final class BidStrategyUpdateDTO
         $this->fill($input);
     }
 
-    public function getBidStrategies(): Bi
+    public function getBidStrategies(): BidStrategyCollection
+    {
+        return $this->bidStrategies;
+    }
+
+    private function validate(array $input): void
+    {
+        if (!isset($input['bid_strategies'])) {
+            throw new ValidationException('Field `bid_strategies` is required.');
+        }
+
+        if (!is_array($input['bid_strategies'])) {
+            throw new ValidationException('Field `bid_strategies` must be an array.');
+        }
+
+        foreach ($input['bid_strategies'] as $bidStrategy) {
+            $this->validateBidStrategy($bidStrategy);
+        }
+    }
+
+    private function validateBidStrategy(array $input): void
+    {
+        if (!isset($input['id'])) {
+            throw new ValidationException('Field `id` is required.');
+        }
+
+        if (!isset($input['details'])) {
+            throw new ValidationException('Field `details` is required.');
+        }
+
+        if (!is_array($input['details'])) {
+         
