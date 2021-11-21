ea10405af227ce09f666631a936a6bab0e6bd324
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-21 09:17:50 +0000

    Corrected typo in README

--- src/Application/DTO/ConversionEventUpdateDTO.php
+++ src/Application/DTO/ConversionEventUpdateDTO.php
@@ -4,4 +4,28 @@ declare(strict_types=1);
 
 namespace App\Application\DTO;
 
-use App\Application\E
+use App\Application\Exception\ValidationException;
+use App\Domain\Model\ConversionEvent;
+use App\Domain\Model\Event;
+use App\Domain\Model\EventCollection;
+use App\Domain\ValueObject\EventType;
+use App\Domain\ValueObject\Id;
+use App\Domain\ValueObject\PaymentStatus;
+use App\Lib\DateTimeHelper;
+use DateTimeInterface;
+
+final class ConversionEventUpdateDTO extends EventUpdateDTO
+{
+    protected function createEventCollection(DateTimeInterface $timeStart, DateTimeInterface $timeEnd): EventCollection
+    {
+        return new EventCollection(EventType::createConversion(), $timeStart, $timeEnd);
+    }
+
+    protected function createEventModel(array $input): Event
+    {
+        return new ConversionEvent(
+            new Id($input['id']),
+            DateTimeHelper::fromTimestamp($input['time']),
+            $this->createImpressionCaseModel($input),
+            new Id($input['group_id']),
+            new Id($input['conversion_
