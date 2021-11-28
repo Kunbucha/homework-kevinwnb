e48621f598c2fc953eb8f86986dfe306104364ce
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-11-28 14:25:24 +0000

    fixed type error

--- src/Domain/Model/CampaignCost.php
+++ src/Domain/Model/CampaignCost.php
@@ -22,4 +22,38 @@ final class CampaignCost
 
     public function __construct(
         int $reportId,
-        Id $ca
+        Id $campaignId,
+        ?float $score = null,
+        int $maxCpm = 0,
+        float $cpmFactor = 1.0,
+        int $views = 0,
+        int $viewsCost = 0,
+        int $clicks = 0,
+        int $clicksCost = 0,
+        int $conversions = 0,
+        int $conversionsCost = 0
+    ) {
+        $this->reportId = $reportId;
+        $this->campaignId = $campaignId;
+        $this->score = $score;
+        $this->maxCpm = $maxCpm;
+        $this->cpmFactor = $cpmFactor;
+        $this->views = $views;
+        $this->viewsCost = $viewsCost;
+        $this->clicks = $clicks;
+        $this->clicksCost = $clicksCost;
+        $this->conversions = $conversions;
+        $this->conversionsCost = $conversionsCost;
+    }
+
+    public function getReportId(): int
+    {
+        return $this->reportId;
+    }
+
+    public function getCampaignId(): Id
+    {
+        return $this->campaignId;
+    }
+
+   
