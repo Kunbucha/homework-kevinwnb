b97c20943e59c87e25b37e5873637ba984b41267
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-12-07 17:26:46 +0000

    Fix another bug

--- src/Domain/Model/Impression.php
+++ src/Domain/Model/Impression.php
@@ -17,3 +17,52 @@ final class Impression
 
     /** @var Id */
     private $userId;
+
+    /** @var Context */
+    private $context;
+
+    public function __construct(
+        Id $id,
+        Id $trackingId,
+        Id $userId,
+        Context $context
+    ) {
+        $this->id = $id;
+        $this->trackingId = $trackingId;
+        $this->userId = $userId;
+        $this->context = $context;
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getTrackingId(): Id
+    {
+        return $this->trackingId;
+    }
+
+    public function getUserId(): Id
+    {
+        return $this->userId;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+
+    public function getContextData(): array
+    {
+        return $this->context->getData();
+    }
+
+    public function getKeywords(): array
+    {
+        return $this->context->getKeywords();
+    }
+
+    public function getHumanScore(): float
+    {
+        return
