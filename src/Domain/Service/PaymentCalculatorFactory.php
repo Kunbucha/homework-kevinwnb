b44a39e98bc220bc162af04dc0fc87cf2283f08c
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-12-13 17:23:19 +0000

    Update spaggiari.py

--- src/Domain/Service/PaymentCalculatorFactory.php
+++ src/Domain/Service/PaymentCalculatorFactory.php
@@ -13,4 +13,30 @@ class PaymentCalculatorFactory
 {
     private CampaignRepository $campaignRepository;
 
-    pri
+    private BidStrategyRepository $bidStrategyRepository;
+
+    private CampaignCostRepository $campaignCostRepository;
+
+    private PaymentCalculatorConfig $config;
+
+    public function __construct(
+        CampaignRepository $campaignRepository,
+        BidStrategyRepository $bidStrategyRepository,
+        CampaignCostRepository $campaignCostRepository,
+        PaymentCalculatorConfig $config
+    ) {
+        $this->campaignRepository = $campaignRepository;
+        $this->bidStrategyRepository = $bidStrategyRepository;
+        $this->campaignCostRepository = $campaignCostRepository;
+        $this->config = $config;
+    }
+
+    public function createPaymentCalculator(): PaymentCalculator
+    {
+        $campaigns = $this->campaignRepository->fetchAll();
+        $bidStrategies = $this->bidStrategyRepository->fetchAll();
+
+        return new PaymentCalculator(
+            $campaigns,
+            $bidStrategies,
+            $this->campaignCostRepository
