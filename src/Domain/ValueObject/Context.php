422993e3fdaa6b63501b4116422f425ba7298e22
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2021-12-26 22:10:03 +0000

    fixed cs issues

--- src/Domain/ValueObject/Context.php
+++ src/Domain/ValueObject/Context.php
@@ -25,4 +25,46 @@ class Context
         if ($humanScore < 0 || $humanScore > 1) {
             throw InvalidArgumentException::fromArgument(
                 'human score',
-           
+                (string)$humanScore,
+                'Must be in the range of <0, 1>.'
+            );
+        }
+        if (($pageRank < 0 && $pageRank !== -1.0) || $pageRank > 1) {
+            throw InvalidArgumentException::fromArgument(
+                'page rank',
+                (string)$pageRank,
+                'Must be in the range of <0, 1> or equal -1.'
+            );
+        }
+
+        $this->humanScore = $humanScore;
+        $this->pageRank = $pageRank;
+        $this->keywords = $keywords;
+        $this->data = $data;
+    }
+
+    public function getHumanScore(): float
+    {
+        return $this->humanScore;
+    }
+
+    public function getPageRank(): float
+    {
+        return $this->pageRank;
+    }
+
+    public function getKeywords(): array
+    {
+        return $this->keywords;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function get(string ...$keys)
+    {
+        $items = $this->data;
+        foreach ($keys as $key) {
+            if (!is_array
