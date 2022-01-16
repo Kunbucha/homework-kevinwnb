e044b4daf73c30873a78eaaad1785cd9d5c046f4
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-01-16 13:23:28 +0000

    Clean up imports.

--- src/Infrastructure/Mapper/CampaignCostMapper.php
+++ src/Infrastructure/Mapper/CampaignCostMapper.php
@@ -5,4 +5,31 @@ declare(strict_types=1);
 namespace App\Infrastructure\Mapper;
 
 use App\Domain\Model\CampaignCost;
-use App\Domain\ValueObject\Id
+use App\Domain\ValueObject\Id;
+use Doctrine\DBAL\Types\Types;
+
+class CampaignCostMapper
+{
+    public static function table(): string
+    {
+        return 'campaign_costs';
+    }
+
+    public static function map(CampaignCost $campaignCost): array
+    {
+        return [
+            'report_id' => $campaignCost->getReportId(),
+            'campaign_id' => $campaignCost->getCampaignId()->toBin(),
+            'score' => $campaignCost->getScore(),
+            'max_cpm' => $campaignCost->getMaxCpm(),
+            'cpm_factor' => $campaignCost->getCpmFactor(),
+            'views' => $campaignCost->getViews(),
+            'views_cost' => $campaignCost->getViewsCost(),
+            'clicks' => $campaignCost->getClicks(),
+            'clicks_cost' => $campaignCost->getClicksCost(),
+            'conversions' => $campaignCost->getConversions(),
+            'conversions_cost' => $campaignCost->getConversionsCost(),
+        ];
+    }
+
+    public static
