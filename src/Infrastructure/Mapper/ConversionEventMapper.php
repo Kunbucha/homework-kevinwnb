e174c0c1f97564df9cefc452f8d66da6112fddc1
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-01-18 17:04:12 +0000

    Modified README.md.

--- src/Infrastructure/Mapper/ConversionEventMapper.php
+++ src/Infrastructure/Mapper/ConversionEventMapper.php
@@ -9,4 +9,33 @@ use App\Domain\Model\Event;
 use App\Domain\ValueObject\EventType;
 use Doctrine\DBAL\Types\Types;
 
-class ConversionEventMapper e
+class ConversionEventMapper extends EventMapper
+{
+    public static function table(): string
+    {
+        return 'conversion_events';
+    }
+
+    public static function map(Event $event): array
+    {
+        /* @var $event ConversionEvent */
+        return array_merge(
+            parent::map($event),
+            [
+                'group_id' => $event->getGroupId()->toBin(),
+                'conversion_id' => $event->getConversionId()->toBin(),
+                'conversion_value' => $event->getConversionValue(),
+                'payment_status' => $event->getPaymentStatus()->getStatus(),
+            ]
+        );
+    }
+
+    public static function types(): array
+    {
+        return array_merge(
+            parent::types(),
+            [
+                'group_id' => Types::BINARY,
+                'conversion_id' => Types::BINARY,
+                'conversion_value' => Types::INTEGER,
+                'payment_status' =
