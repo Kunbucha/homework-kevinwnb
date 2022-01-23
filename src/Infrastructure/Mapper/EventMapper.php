398b04c5b3fc8fd66b19abf01d73b5bfe732f250
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-01-23 14:41:59 +0000

    minor documentation changes

--- src/Infrastructure/Mapper/EventMapper.php
+++ src/Infrastructure/Mapper/EventMapper.php
@@ -13,4 +13,21 @@ abstract class EventMapper
 
     abstract protected static function getEventType(): string;
 
-    public static func
+    public static function map(Event $event): array
+    {
+        return [
+            'id' => $event->getId()->toBin(),
+            'time' => $event->getTime(),
+            'case_id' => $event->getCaseId()->toBin(),
+            'case_time' => $event->getCaseTime(),
+            'publisher_id' => $event->getPublisherId()->toBin(),
+            'zone_id' => $event->getZoneId() !== null ? $event->getZoneId()->toBin() : null,
+            'advertiser_id' => $event->getAdvertiserId()->toBin(),
+            'campaign_id' => $event->getCampaignId()->toBin(),
+            'banner_id' => $event->getBannerId()->toBin(),
+            'impression_id' => $event->getImpressionId()->toBin(),
+            'tracking_id' => $event->getTrackingId()->toBin(),
+            'user_id' => $event->getUserId()->toBin(),
+            'human_score' => $event->getHumanScore(),
+            'page_rank' => $event->getPageRank(),
+            'keywords' => $event->ge
