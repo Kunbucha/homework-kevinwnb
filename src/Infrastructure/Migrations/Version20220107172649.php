667e2d55c4bb67b6aebea759058d19b2d01815ce
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-02-21 10:40:16 +0000

    ipc/shm: Fix shmat mmap nil-page protection

--- src/Infrastructure/Migrations/Version20220107172649.php
+++ src/Infrastructure/Migrations/Version20220107172649.php
@@ -24,4 +24,25 @@ CREATE TABLE campaign_costs
     updated_at       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     report_id        BIGINT(20)      NOT NULL,
     campaign_id      VARBINARY(16)   NOT NULL,
-    score   
+    score            FLOAT           DEFAULT NULL,
+    max_cpm          BIGINT UNSIGNED NOT NULL,
+    cpm_factor       FLOAT           NOT NULL,
+    views            INT UNSIGNED    NOT NULL,
+    views_cost       BIGINT UNSIGNED NOT NULL,
+    clicks           INT UNSIGNED    NOT NULL,
+    clicks_cost      BIGINT UNSIGNED NOT NULL,
+    conversions      INT UNSIGNED    NOT NULL,
+    conversions_cost BIGINT UNSIGNED NOT NULL,
+    INDEX campaign_costs_updated_at (updated_at),
+    CONSTRAINT campaign_costs_report_id_campaign_id_index
+        UNIQUE (report_id, campaign_id)
+);
+SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE campaign_costs');
+    }
+}
---
+++ src/Infrastructure/Repository/DoctrineBidStrategyRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Repository;
+
+use App\Domain\Exception\DomainRepositoryException;
+use App\Domain\Model\BidStrategy;
+use App\Domain\Model\BidStrateg
