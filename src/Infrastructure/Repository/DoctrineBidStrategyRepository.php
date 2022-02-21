53f5244469fc95b2d122bf8ee83ea3f3c310d6b7
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-02-21 18:32:28 +0000

    Create greetings.yml

--- src/Infrastructure/Repository/DoctrineBidStrategyRepository.php
+++ src/Infrastructure/Repository/DoctrineBidStrategyRepository.php
@@ -6,4 +6,30 @@ namespace App\Infrastructure\Repository;
 
 use App\Domain\Exception\DomainRepositoryException;
 use App\Domain\Model\BidStrategy;
-use App\Domain\Model\BidStrateg
+use App\Domain\Model\BidStrategyCollection;
+use App\Domain\Repository\BidStrategyRepository;
+use App\Domain\ValueObject\IdCollection;
+use App\Infrastructure\Mapper\BidStrategyMapper;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception as DBALException;
+
+final class DoctrineBidStrategyRepository extends DoctrineModelUpdater implements BidStrategyRepository
+{
+    public function saveAll(BidStrategyCollection $bidStrategies): int
+    {
+        if (0 === $bidStrategies->count()) {
+            return 0;
+        }
+
+        $count = 0;
+
+        $mapIds = [];
+        foreach ($bidStrategies as $bidStrategy) {
+            /*  @var $bidStrategy BidStrategy */
+            $id = $bidStrategy->getId()->toBin();
+            if (!isset($mapIds[$id])) {
+                $mapIds[$id] = 1;
+            }
+        }
+        $ids = array_keys($mapIds);
+        $deleteQuery = sprintf('DELETE FROM %s WHERE bid_strategy_
