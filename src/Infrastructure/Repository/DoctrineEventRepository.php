1bb2ffd938226137cf410d73bc0dd4831783a4e5
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-03-06 08:47:46 +0000

    Resize

--- src/Infrastructure/Repository/DoctrineEventRepository.php
+++ src/Infrastructure/Repository/DoctrineEventRepository.php
@@ -10,4 +10,30 @@ use App\Domain\Model\Event;
 use App\Domain\Model\EventCollection;
 use App\Domain\Repository\EventRepository;
 use App\Domain\ValueObject\EventType;
-use App\Infrastructur
+use App\Infrastructure\Mapper\ClickEventMapper;
+use App\Infrastructure\Mapper\ConversionEventMapper;
+use App\Infrastructure\Mapper\EventMapper;
+use App\Infrastructure\Mapper\ViewEventMapper;
+use DateTimeInterface;
+use Doctrine\DBAL\Driver\Exception as DBALDriverException;
+use Doctrine\DBAL\Exception as DBALException;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\DBAL\Types\Types;
+
+final class DoctrineEventRepository extends DoctrineModelUpdater implements EventRepository
+{
+    public function saveAll(
+        EventCollection $events
+    ): int {
+        return $this->upsertEvents($events, self::getMapper($events->getType()));
+    }
+
+    public function deleteByTime(
+        EventType $type,
+        ?DateTimeInterface $timeStart = null,
+        ?DateTimeInterface $timeEnd = null
+    ): int {
+        /*  @var $mapper EventMapper */
+        $mapper = self::getMapper($type);
+        try {
+            return $this->clearInterval($mapper::table(), $timeStar
