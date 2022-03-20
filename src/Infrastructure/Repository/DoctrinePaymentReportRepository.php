3999a1a21faf28352123be1d60afc88b9a143387
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-03-20 16:54:46 +0000

    update error message

--- src/Infrastructure/Repository/DoctrinePaymentReportRepository.php
+++ src/Infrastructure/Repository/DoctrinePaymentReportRepository.php
@@ -16,4 +16,33 @@ use Doctrine\DBAL\Exception as DBALException;
 
 final class DoctrinePaymentReportRepository extends DoctrineModelUpdater implements PaymentReportRepository
 {
-    p
+    public function fetch(int $id): ?PaymentReport
+    {
+        try {
+            $result =
+                $this->db->fetchAssociative(
+                    sprintf('SELECT * FROM %s WHERE id = ?', PaymentReportMapper::table()),
+                    [$id]
+                );
+        } catch (DBALException $exception) {
+            throw new DomainRepositoryException($exception->getMessage());
+        }
+
+        return $result !== false ? PaymentReportMapper::fill($result) : null;
+    }
+
+    public function fetchOrCreate(int $id): PaymentReport
+    {
+        if (null === ($report = $this->fetch($id))) {
+            $report = new PaymentReport($id, PaymentReportStatus::createIncomplete());
+            $this->save($report);
+        }
+        return $report;
+    }
+
+    public function fetchAll(): PaymentReportCollection
+    {
+        return $this->fetchQuery();
+    }
+
+    public function fetchById(int ...$ids): PaymentR
