d5889b3ffbac7b43f3269d1cc34083cc4ead3b15
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-04-03 15:35:12 +0000

    line spacing

--- src/Infrastructure/Repository/DoctrinePaymentRepository.php
+++ src/Infrastructure/Repository/DoctrinePaymentRepository.php
@@ -2,4 +2,29 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Reposi
+namespace App\Infrastructure\Repository;
+
+use App\Domain\Exception\DomainRepositoryException;
+use App\Domain\Repository\PaymentRepository;
+use App\Infrastructure\Mapper\PaymentMapper;
+use Doctrine\DBAL\Driver\Exception as DBALDriverException;
+use Doctrine\DBAL\Exception as DBALException;
+
+final class DoctrinePaymentRepository extends DoctrineModelUpdater implements PaymentRepository
+{
+    public function saveAllRaw(int $reportId, array $payments): int
+    {
+        foreach ($payments as $key => $payment) {
+            $payment['report_id'] = $reportId;
+            $payments[$key] = PaymentMapper::map($payment);
+        }
+
+        try {
+            return $this->insertBatch(
+                PaymentMapper::table(),
+                $payments,
+                PaymentMapper::types()
+            );
+        } catch (DBALException $exception) {
+            throw new DomainRepositoryException($exception->getMessage());
+      
