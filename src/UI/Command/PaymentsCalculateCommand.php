506ddf4d05fe4500146b3b89b35cd85406e5ba3d
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-04-10 22:27:36 +0000

    minor UI changes

--- src/UI/Command/PaymentsCalculateCommand.php
+++ src/UI/Command/PaymentsCalculateCommand.php
@@ -45,3 +45,31 @@ class PaymentsCalculateCommand extends Command
             ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force calculation of incomplete report');
     }
 
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if (!$this->lock()) {
+            $io->warning('The command is already running in another process.');
+
+            return self::FAILURE;
+        }
+
+        $date = $input->getArgument('date');
+        if ($date === null) {
+            $this->calculateAll($io);
+        } else {
+            if (preg_match('/^\d+$/', $date)) {
+                $timestamp = (int)$date;
+            } else {
+                try {
+                    $timestamp = (DateTimeHelper::fromString($date)->getTimestamp());
+                } catch (DateTimeException $exception) {
+                    $io->error($exception->getMessage());
+                    $this->release();
+
+                    return self::FAILURE;
+                }
+            }
+
+            $this->calculate($timestamp, $input->getOption('force'), $io)
