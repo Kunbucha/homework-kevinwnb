45165671331ce034f4be08bacf73172b4634d049
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-04-24 20:10:09 +0000

    Add a missing import

--- src/UI/Controller/EventController.php
+++ src/UI/Controller/EventController.php
@@ -7,3 +7,29 @@ namespace App\UI\Controller;
 use App\Application\Command\EventUpdateCommand;
 use App\Application\DTO\ClickEventUpdateDTO;
 use App\Application\DTO\ConversionEventUpdateDTO;
+use App\Application\DTO\EventUpdateDTO;
+use App\Application\DTO\ViewEventUpdateDTO;
+use App\Application\Exception\ValidationException;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+class EventController extends AbstractController
+{
+    private EventUpdateCommand $eventUpdateCommand;
+
+    private LoggerInterface $logger;
+
+    public function __construct(EventUpdateCommand $eventUpdateCommand, LoggerInterface $logger)
+    {
+        $this->eventUpdateCommand = $eventUpdateCommand;
+        $this->logger = $logger;
+    }
+
+    private function parseRequest(Request $request, string $dto): EventUpdateDTO
+    {
+        $input = json_decode($request->getContent(), true);
+     
