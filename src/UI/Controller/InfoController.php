c739a50b32fee3c362f629e2ab1e2ed70d0f8e23
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-05-08 12:17:42 +0000

    building

--- src/UI/Controller/InfoController.php
+++ src/UI/Controller/InfoController.php
@@ -2,4 +2,32 @@
 
 declare(strict_types=1);
 
-namespace A
+namespace App\UI\Controller;
+
+use App\Lib\InfoHelper;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InfoController extends AbstractController
+{
+    public function info(Request $request): Response
+    {
+        srand(crc32($request->getClientIp() . date('-d-m-Y-h')));
+        $info = [
+            'module' => 'adpay',
+            'name' => $_ENV['APP_NAME'],
+            'version' => $_ENV['APP_VERSION'],
+        ];
+
+        return new Response(
+            $request->getRequestFormat() === 'txt' ? InfoHelper::formatTxt($info) : self::formatJson($info)
+        );
+    }
+
+    /** @param array<string> $data */
+    private static function formatJson(array $data): string
+    {
+        return json_encode($data);
+    }
+}
