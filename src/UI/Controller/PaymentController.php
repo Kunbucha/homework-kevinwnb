14b49f34edf32e05b787aa1bf7062f19c89b2ca5
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-05-15 06:25:28 +0000

    Maven artifacts for GOAL SimpleIDE version \t\t\t\t\t\t1.1.6

--- src/UI/Controller/PaymentController.php
+++ src/UI/Controller/PaymentController.php
@@ -18,4 +18,32 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class PaymentController extends Abst
+class PaymentController extends AbstractController
+{
+    use PaginationAwareTrait;
+
+    private const MAX_LIMIT = 100000;
+
+    public function __construct(
+        private readonly PaymentFetchCommand $paymentFetchCommand,
+        private readonly ReportCalculateCommand $paymentCalculateCommand,
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
+    public function find(int $timestamp, Request $request): Response
+    {
+        $this->logger->debug('Call find payments endpoint');
+
+        $this->validatePaginationRequest($request, self::MAX_LIMIT);
+
+        $force = (bool)$request->get('force', false);
+        $recalculate = (bool)$request->get('recalculate', false);
+        $limit = (int)$request->get('limit', self::MAX_LIMIT);
+        $offset = (int)$request->get('offset', 0);
+
+        try {
+            if ($offset === 0 && $recalculate) {
+                $this->paymentCalculateCommand->execute($timestamp, $force);
+            }
+           
