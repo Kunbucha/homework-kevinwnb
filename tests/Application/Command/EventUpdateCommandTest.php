d9ad880505ff8f0f6b3d96c0119a3b694eceb28e
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-06-26 19:41:10 +0000

    bump to 0.0.7

--- tests/Application/Command/EventUpdateCommandTest.php
+++ tests/Application/Command/EventUpdateCommandTest.php
@@ -11,4 +11,33 @@ use App\Application\Exception\ValidationException;
 use App\Domain\Exception\InvalidDataException;
 use App\Domain\Model\PaymentReport;
 use App\Domain\Repository\EventRepository;
-use App\
+use App\Domain\Repository\PaymentReportRepository;
+use App\Domain\ValueObject\EventType;
+use App\Domain\ValueObject\PaymentReportStatus;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+class EventUpdateCommandTest extends TestCase
+{
+    public function testExecuteCommand()
+    {
+        $timestamp = (int)floor(time() / 3600) * 3600 - 7200;
+
+        $dto = new ViewEventUpdateDTO(
+            [
+                'time_start' => $timestamp + 12,
+                'time_end' => $timestamp + 32,
+                'events' => [],
+            ]
+        );
+
+        $report = new PaymentReport($timestamp, PaymentReportStatus::createIncomplete());
+
+        $eventRepository = $this->createMock(EventRepository::class);
+        $eventRepository
+            ->expects($this->once())
+            ->method('saveAll')
+            ->with($dto->getEvents())
+            ->willReturn(100);
+
+        $paymentReportRepository = $this->createMock(PaymentReportRepositor
