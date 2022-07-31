4b835f08ef7d8f52d76d507020b3d8fe8938c89a
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-07-31 08:53:25 +0000

    Screenshot.

--- tests/Application/Command/ReportFetchCommandTest.php
+++ tests/Application/Command/ReportFetchCommandTest.php
@@ -8,4 +8,30 @@ use App\Application\Command\ReportFetchCommand;
 use App\Domain\Model\PaymentReport;
 use App\Domain\Model\PaymentReportCollection;
 use App\Domain\Repository\PaymentReportRepository;
-use App\Domain\ValueObject\PaymentReport
+use App\Domain\ValueObject\PaymentReportStatus;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+class ReportFetchCommandTest extends TestCase
+{
+    public function testExecuteCommand()
+    {
+        $repository = $this->createMock(PaymentReportRepository::class);
+        $repository->expects($this->once())
+            ->method('fetchById')
+            ->with(100, 101)
+            ->willReturn(
+                new PaymentReportCollection(self::paymentReport(100), self::paymentReport(101))
+            );
+
+        /** @var PaymentReportRepository $repository */
+        $command = new ReportFetchCommand($repository, new NullLogger());
+
+        $this->assertEquals([100, 101], $command->execute(100, 101)->getReportIds());
+    }
+
+    public function testExecuteEmptyCommand()
+    {
+        $repository = $this->createMock(PaymentReportRepository::class);
+        $repository->expects($this->once())
+            ->method('
