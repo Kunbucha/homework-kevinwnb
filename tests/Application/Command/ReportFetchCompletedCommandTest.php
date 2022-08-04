40059ebcf39bc7d1b83705752046fd29defb3e96
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-08-04 05:59:56 +0000

    MAJ

--- tests/Application/Command/ReportFetchCompletedCommandTest.php
+++ tests/Application/Command/ReportFetchCompletedCommandTest.php
@@ -12,4 +12,29 @@ use App\Domain\ValueObject\PaymentReportStatus;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
 
-class ReportFetchCompletedCommandTest extends Te
+class ReportFetchCompletedCommandTest extends TestCase
+{
+    public function testExecuteCommand()
+    {
+        $command = $this->reportFetchCommand();
+        $this->assertEquals([100, 101], $command->execute()->getReportIds());
+    }
+
+    private function reportFetchCommand(): ReportFetchCompletedCommand
+    {
+        $repository = $this->createMock(PaymentReportRepository::class);
+        $repository->expects($this->once())
+            ->method('fetchByStatus')
+            ->with(
+                $this->callback(
+                    function (PaymentReportStatus $item) {
+                        return $item->getStatus() === PaymentReportStatus::COMPLETE;
+                    }
+                )
+            )
+            ->willReturn(
+                new PaymentReportCollection(self::paymentReport(100), self::paymentReport(101))
+            );
+
+        /** @var PaymentReportRepository $repository */
+        return new ReportFetc
