ccd3ed1ba0b674843dc6704e1cc1000c632696ee
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-09-26 19:46:38 +0000

    static pages

--- tests/Application/DTO/PaymentFetchDTOTest.php
+++ tests/Application/DTO/PaymentFetchDTOTest.php
@@ -1,3 +1,37 @@
 <?php
 
-declare(stric
+declare(strict_types=1);
+
+namespace App\Tests\Application\DTO;
+
+use App\Application\DTO\PaymentFetchDTO;
+use PHPUnit\Framework\TestCase;
+
+final class PaymentFetchDTOTest extends TestCase
+{
+    public function testCalculated(): void
+    {
+        $dto = new PaymentFetchDTO(false, []);
+        $this->assertFalse($dto->isCalculated());
+
+        $dto = new PaymentFetchDTO(true, []);
+        $this->assertTrue($dto->isCalculated());
+    }
+
+    public function testEmptyPayments(): void
+    {
+        $dto = new PaymentFetchDTO(false, []);
+        $this->assertEmpty($dto->getPayments());
+    }
+
+    public function testPayments(): void
+    {
+        $dto = new PaymentFetchDTO(false, [self::payment(1), self::payment(2),]);
+        $this->assertCount(2, $dto->getPayments());
+        $this->assertEquals(
+            [
+                [
+                    'event_id' => 'aac567e1396b4cadb52223a51796fdb1',
+                    'event_type' => 'view',
+                    'status' => 1,
+  
