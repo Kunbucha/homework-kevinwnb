4a522d179e12f2e4674665b6ba878a668bf0ae63
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-10-01 11:43:46 +0000

    Update release

--- tests/Domain/Model/BidStrategyCollectionTest.php
+++ tests/Domain/Model/BidStrategyCollectionTest.php
@@ -5,4 +5,36 @@ declare(strict_types=1);
 namespace App\Tests\Domain\Model;
 
 use App\Domain\Model\BidStrategy;
-use 
+use App\Domain\Model\BidStrategyCollection;
+use App\Domain\ValueObject\Id;
+use PHPUnit\Framework\TestCase;
+
+final class BidStrategyCollectionTest extends TestCase
+{
+    public function testMultiplyAdding(): void
+    {
+        $item1 = self::createBidStrategy(1);
+        $item2 = self::createBidStrategy(2);
+        $item3 = self::createBidStrategy(3);
+        $item4 = self::createBidStrategy(4);
+
+        $this->assertCount(4, new BidStrategyCollection($item1, $item2, $item3, $item4));
+    }
+
+    public function testEmptyCollection(): void
+    {
+        $collection = new BidStrategyCollection();
+
+        $this->assertCount(0, $collection);
+        $this->assertEmpty($collection);
+    }
+
+    private static function createBidStrategy(int $id): BidStrategy
+    {
+        return new BidStrategy(
+            new Id(str_pad((string)$id, 32, '0', STR_PAD_LEFT)),
+            'user:country:st',
+            0.99
+        );
+    }
+}
