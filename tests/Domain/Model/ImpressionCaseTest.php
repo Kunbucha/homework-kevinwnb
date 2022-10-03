510ef67e813b13998ee8a0da12c89a0915628301
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-10-03 16:17:31 +0000

    - [X] Release notes

--- tests/Domain/Model/ImpressionCaseTest.php
+++ tests/Domain/Model/ImpressionCaseTest.php
@@ -20,4 +20,33 @@ final class ImpressionCaseTest extends TestCase
         $publisherId = 'ffc567e1396b4cadb52223a51796fd02';
         $zoneId = 'aac567e1396b4cadb52223a51796fdbb';
         $advertiserId = 'bbc567e1396b4cadb52223a51796fdaa';
-        $campaignId = 'ccc567e1396b4cadb52
+        $campaignId = 'ccc567e1396b4cadb52223a51796fdcc';
+        $bannerId = 'ddc567e1396b4cadb52223a51796fddd';
+
+        $impressionId = '13c567e1396b4cadb52223a51796fd03';
+        $trackingId = '23c567e1396b4cadb52223a51796fd02';
+        $userId = '33c567e1396b4cadb52223a51796fd01';
+        $keywords = ['k' => 111];
+        $context = ['a' => 123];
+        $humanScore = 0.89;
+        $pageRank = 0.99;
+
+        $impression = new Impression(
+            new Id($impressionId),
+            new Id($trackingId),
+            new Id($userId),
+            new Context($humanScore, $pageRank, $keywords, $context)
+        );
+
+        $case = new ImpressionCase(
+            new Id($caseId),
+            DateTimeHelper::fromString($caseTime),
+            new Id($publisherId),
+            new Id($zoneId),
+            new Id($advertiserId),
+            new Id($campaignId),
+            new Id($bannerId),
+            $impression
+        );
+
+        $this->assertI
