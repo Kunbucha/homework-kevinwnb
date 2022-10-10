cf385fd4f4b80a53a3ccffd02f982e98e5a4f994
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-10-10 08:48:43 +0000

    xen/hvc: Fix error cases around HVM_PARAM_CONSOLE_PFN

--- tests/Domain/Model/ViewEventTest.php
+++ tests/Domain/Model/ViewEventTest.php
@@ -25,4 +25,30 @@ final class ViewEventTest extends TestCase
         $caseId = '43c567e1396b4cadb52223a51796fd01';
         $caseTime = '2019-01-01T12:00:00+10:00';
         $publisherId = 'ffc567e1396b4cadb52223a51796fd02';
-        $zone
+        $zoneId = 'aac567e1396b4cadb52223a51796fdbb';
+        $advertiserId = 'bbc567e1396b4cadb52223a51796fdaa';
+        $campaignId = 'ccc567e1396b4cadb52223a51796fdcc';
+        $bannerId = 'ddc567e1396b4cadb52223a51796fddd';
+
+        $impressionId = '13c567e1396b4cadb52223a51796fd03';
+        $trackingId = '23c567e1396b4cadb52223a51796fd02';
+        $userId = '33c567e1396b4cadb52223a51796fd01';
+        $keywords = ['k' => 111];
+        $context = ['a' => 123];
+        $humanScore = 0.89;
+        $pageRank = 0.99;
+
+        $impression = new Impression(
+            new Id($impressionId),
+            new Id($trackingId),
+            new Id($userId),
+            new Context($humanScore, $pageRank, $keywords, $context)
+        );
+
+        $case = new ImpressionCase(
+            new Id($caseId),
+            DateTimeHelper::fromString($caseTime),
+            new Id($publisherId),
+            new Id($zoneId),
+            new Id($advertiserId),
+          
