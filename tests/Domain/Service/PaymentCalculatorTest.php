7c46cbc2ce608aec4462d720b27c98796281ee1e
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2022-10-16 11:53:49 +0000

    upgrade lodash

--- tests/Domain/Service/PaymentCalculatorTest.php
+++ tests/Domain/Service/PaymentCalculatorTest.php
@@ -11,4 +11,33 @@ use App\Domain\Model\BidStrategyCollection;
 use App\Domain\Model\Campaign;
 use App\Domain\Model\CampaignCollection;
 use App\Domain\Model\CampaignCost;
-use App\Domain\Model\CampaignCostCollecti
+use App\Domain\Model\CampaignCostCollection;
+use App\Domain\Model\Conversion;
+use App\Domain\Model\ConversionCollection;
+use App\Domain\Repository\CampaignCostRepository;
+use App\Domain\Service\PaymentCalculator;
+use App\Domain\ValueObject\BannerType;
+use App\Domain\ValueObject\Budget;
+use App\Domain\ValueObject\EventType;
+use App\Domain\ValueObject\Id;
+use App\Domain\ValueObject\LimitType;
+use App\Domain\ValueObject\Medium;
+use App\Domain\ValueObject\PaymentCalculatorConfig;
+use App\Domain\ValueObject\PaymentStatus;
+use App\Lib\DateTimeHelper;
+use DateTimeInterface;
+use PHPUnit\Framework\TestCase;
+
+final class PaymentCalculatorTest extends TestCase
+{
+    private const TIME = 1571231623;
+
+    private const ADVERTISER_ID = '50000000000000000000000000000001';
+
+    private const CAMPAIGN_ID = '60000000000000000000000000000001';
+
+    private const CAMPAIGN_BUDGET = 10000000000;
+
+    private const CAMPAIGN_CPV = 100;
+
+    
