4b79901b94bccb06e68afff17ffe5e3cb8c9859c
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2023-03-08 20:18:24 +0000

    Update Makefile

--- tests/Domain/ValueObject/IdTest.php
+++ tests/Domain/ValueObject/IdTest.php
@@ -20,4 +20,38 @@ final class IdTest extends TestCase
         $this->assertEquals(hex2bin($value), $id->toBin());
     }
 
-    public fun
+    public function testInvalidEmptyId(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Id('');
+    }
+
+    public function testInvalidId(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Id('1234qwe');
+    }
+
+    public function testInvalidShortId(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Id('43c567e1396b4cadb52223a');
+    }
+
+    public function testIsEquals(): void
+    {
+        $value = '43c567e1396b4cadb52223a51796fd01';
+
+        $id = new Id($value);
+        $id2 = new Id($value);
+        $id3 = new Id('43c567e1396b4cadb52223a51796fd02');
+
+        $this->assertTrue($id->equals($id));
+        $this->assertTrue($id->equals($id2));
+        $this->assertFalse($id->equals($id3));
+        $this->assertFalse($id2->equals($id3));
+    }
+
+    public function testFromBin(): void
+    {
+        $value = '43c567
