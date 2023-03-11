1ce8a24e47cc83acfa4d27077b5b4f822740545d
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2023-03-11 19:56:43 +0000

    fix linter error

--- tests/Domain/ValueObject/PaymentStatusTest.php
+++ tests/Domain/ValueObject/PaymentStatusTest.php
@@ -17,4 +17,27 @@ final class PaymentStatusTest extends TestCase
         $this->assertNull($status->getStatus());
         $this->assertFalse($status->isProcessed());
         $this->assertFalse($status->isAccepted());
-        $this->assertFalse($status->isR
+        $this->assertFalse($status->isRejected());
+        $this->assertEquals('unprocessed', $status->toString());
+        $this->assertEquals('unprocessed', (string)$status);
+    }
+
+    public function testAcceptedStatus(): void
+    {
+        $status = new PaymentStatus(PaymentStatus::ACCEPTED);
+
+        $this->assertEquals(PaymentStatus::ACCEPTED, $status->getStatus());
+        $this->assertTrue($status->isProcessed());
+        $this->assertTrue($status->isAccepted());
+        $this->assertFalse($status->isRejected());
+        $this->assertEquals('accepted', $status->toString());
+    }
+
+    public function testUnknownStatus(): void
+    {
+        $status = new PaymentStatus(PHP_INT_MAX);
+
+        $this->assertTrue($status->isProcessed());
+        $this->assertFalse($status->isAccepted());
+        $this->assertTrue($status->isRejected());
+        $this->assertEquals('rejected:unknown', $status->toString());
