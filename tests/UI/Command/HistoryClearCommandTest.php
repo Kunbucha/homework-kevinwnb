1f765216b88348a7c04a6892739dd3cd2833a3e4
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2023-03-22 19:50:35 +0000

    Removed .pyc files

--- tests/UI/Command/HistoryClearCommandTest.php
+++ tests/UI/Command/HistoryClearCommandTest.php
@@ -21,4 +21,32 @@ use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Lock\Store\FlockStore;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 
-final class HistoryClearCommandTest exten
+final class HistoryClearCommandTest extends CommandTestCase
+{
+    protected static $command = 'ops:history:clear';
+
+    public function testExecute(): void
+    {
+        $this->setUpReports(3);
+        $this->setUpEvents(5);
+        $this->executeCommand([], 0, '3 payment reports removed', '5 events removed');
+    }
+
+    public function testEmptyExecute(): void
+    {
+        $this->executeCommand([], 0, '0 payment reports removed', '0 events removed');
+    }
+
+    public function testPeriod(): void
+    {
+        $this->executeCommand([], 0, 'Clearing payments and events older than ' . self::periodToDate(48 * 3600));
+        $this->executeCommand(
+            ['--period' => 'PT0H'],
+            0,
+            'Clearing payments and events older than ' . self::periodToDate(0)
+        );
+        $this->executeCommand(
+            ['--period' => 'P1D'],
+            0,
+            'Clearing payments and events older than ' . self::periodToDate(24 * 3600)
+        )
