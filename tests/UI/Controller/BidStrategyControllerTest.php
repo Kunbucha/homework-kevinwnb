dd819a9c5f3633301d15847b7df0fb6c72a82d96
Kunbucha <107943861+Kunbucha@users.noreply.github.com>
2023-04-02 18:18:17 +0000

    SCSI: zfcp: remove invalid reference to list iterator variable

--- tests/UI/Controller/BidStrategyControllerTest.php
+++ tests/UI/Controller/BidStrategyControllerTest.php
@@ -13,4 +13,28 @@ final class BidStrategyControllerTest extends WebTestCase
         $parameters = [
             'bid_strategies' => [
                 [
-                    'id' => 'fff567e1396b4cadb52223a51796f
+                    'id' => 'fff567e1396b4cadb52223a51796fd02',
+                    'name' => 'Test bid strategy',
+                    'details' => [
+                        [
+                            'category' => 'user:country:st',
+                            'rank' => 0.98,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $client = self::createClient();
+        $client->request('POST', '/api/v1/bid-strategies', [], [], [], json_encode($parameters));
+        $this->assertEquals(204, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEmptyUpdateBidStrategy(): void
+    {
+        $client = self::createClient();
+        $client->request('POST', '/api/v1/bid-strategies');
+        $this->assertEquals(422, $client->getResponse()->getStatusCode());
+
+        $client->request('POST', '/api/v1/bid-strategies', [], [], [], json_encode([]));
+        $this->assertEquals(422, $client->getResponse()->getStatusCo
